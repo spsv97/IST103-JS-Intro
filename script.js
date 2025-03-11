49bec1783c6ff33f1e063bb506bc7d6c89f7ad9e
Steven <steven.sandovalv@gmail.com>
2025-03-10 20:22:12 -0700

    Steven - Class Activity 2 - 10 March 2025 #1

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-alert("Hello world")
+// alert("Hello world")
 
 function clickHeading(){
     alert("Heading is beign clicked")
 }
+
+let name="Steven"
+let age=27
+let student=false
+let france=undefined
+let english=null
+
+const nacionality="Ecuadorian"
+
+console.log("The name of the person is:" + name)
+console.log("The age of the person is:" + age)
+console.log("is Steven a student?:" + student)
+console.log("Level of french:" + france)
+console.log("Level of english:" + english)
